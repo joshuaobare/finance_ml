d023bd232dd95e3cf619017628dbc0a9506d9c11
Joshua Obare <jobare.obare@gmail.com>
2024-06-06 14:33:03 +0300

    Add code to build ARIMA model

--- arima-model.py
+++ arima-model.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error
 
 data = pd.read_csv("./data/SP.csv")
 data["Date"] = pd.to_datetime(data["Date"])
@@ -12,4 +16,16 @@ plt.plot(data["High"])
 plt.title("S&P Trend")
 plt.xlabel("Date")
 plt.ylabel("Price in USD")
+#plt.show()
+
+adf_test = adfuller(data["High"])
+print('ADF Statistic: %f' % adf_test[0])
+print('p-value: %f' % adf_test[1])
+plot_pacf(data["High"], lags=40)
+plot_acf(data["High"], lags=40)
 plt.show()
+
+# ARIMA Model Building
+model = ARIMA(data["High"], order=(1,0,1))
+model_fit = model.fit()
+forecast = model_fit.get_forecast(steps=30)
