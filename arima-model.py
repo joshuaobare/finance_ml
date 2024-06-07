22788e6b7a8b5bd872c5894521eef1797aa17c82
Joshua Obare <jobare.obare@gmail.com>
2024-06-07 14:06:47 +0300

    Add function to check stationarity

--- arima-model.py
+++ arima-model.py
@@ -5,26 +5,98 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
 
-data = pd.read_csv("./data/SP.csv")
-data["Date"] = pd.to_datetime(data["Date"])
+data = pd.read_csv("./data/SP.csv",index_col="Date")
+data.fillna(0)
+data = data[:(int(.3 * len(data)))]
+print(data)
+data.index = pd.to_datetime(data.index, format="%b %d, %Y")
 
 # Remove any commas on the Highs column
-data["High"]= data["High"].replace(",","", regex=True)
+data["High"] = data["High"].replace(",","", regex=True)
 data["High"] = pd.to_numeric(data["High"])
-data.set_index('Date', inplace = True)
+#print(data["High"])
+
 plt.plot(data["High"])
 plt.title("S&P Trend")
 plt.xlabel("Date")
 plt.ylabel("Price in USD")
 #print(data["High"])
-#plt.show()
+plt.show()
 
+def test_stationarity(timeseries):
+    #Determing rolling statistics
+    rolmean = timeseries.rolling(12).mean()
+    rolstd = timeseries.rolling(12).std()
+    #Plot rolling statistics:
+    plt.plot(timeseries, color='blue',label='Original')
+    plt.plot(rolmean, color='red', label='Rolling Mean')
+    plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean and Standard Deviation')
+    plt.show()
+    print("Results of dickey fuller test")
+    adft = adfuller(timeseries,autolag='AIC')
+    # output for dft will give us without defining what the values are.
+    #hence we manually write what values does it explains using a for loop
+    output = pd.Series(adft[0:4],index=['Test Statistics','p-value','No. of lags used','Number of observations used'])
+    for key,values in adft[4].items():
+        output['critical value (%s)'%key] =  values
+    print(output)
+test_stationarity(data["High"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 adf_test = adfuller(data["High"])
 print('ADF Statistic: %f' % adf_test[0])
 print('p-value: %f' % adf_test[1])
-#plot_pacf(data["High"], lags=40)
-#plot_acf(data["High"], lags=40)
-#plt.show()
+plot_pacf(data["High"], lags=40)
+plot_acf(data["High"], lags=40)
+plt.show()
 
 # ARIMA Model Building
 model = ARIMA(data["High"], order=(1,0,1))
@@ -36,7 +108,6 @@ forecast = model_fit.get_forecast(steps=30)
 # Train and test data
 train_size = int(len(data) *0.8)
 train, test = data[0:train_size], data[train_size:len(data)]
-test["High"].to_csv("SPtest.csv")
 
 # Fit ARIMA model onto the training dataset
 model_train = ARIMA(train["High"], order= (1,0,1))
@@ -66,3 +137,4 @@ plt.legend()
 plt.show()
 
 print("RMSE: ", rmse )
+'''
