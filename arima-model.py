1567184b4fb43068a281570f3f49c70af098072b
joshuaobare <jobare.obare@gmail.com>
2024-06-08 18:02:32 +0300

    Add code to find the best parameters via the auto_arima function

--- arima-model.py
+++ arima-model.py
@@ -76,9 +76,22 @@ plt.legend()
 plt.show()
 
 
+model_autoARIMA = auto_arima(train_data, start_p=0, start_q=0,
+                      test='adf',       # use adftest to find optimal 'd'
+                      max_p=3, max_q=3, # maximum p and q
+                      m=1,              # frequency of series
+                      d=None,           # let model determine 'd'
+                      seasonal=False,   # No Seasonality
+                      start_P=0, 
+                      D=0, 
+                      trace=True,
+                      error_action='ignore',  
+                      suppress_warnings=True, 
+                      stepwise=True)
 
-
-
+print(model_autoARIMA.summary())
+model_autoARIMA.plot_diagnostics(figsize=(15,8))
+plt.show()
 
 
 
