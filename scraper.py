778a0e79d90fc366c90a9d19d49a733e35c0f609
joshuaobare <jobare.obare@gmail.com>
2024-06-25 20:36:31 +0300

    Add function to find scraper start date

--- scraper.py
+++ scraper.py
@@ -8,17 +8,28 @@ class Scraper:
         self.url = url
         self.header = ['Date',	'Open',	'High',	'Low',	'Close*',	'Adj Close**',	'Volume']
         self.all_data = []
+        self.start_date = None
+        self.find_start_date()
         self.fetchData()
         self.write_to_file()
+
+    def find_start_date(self):
+        file_name = self.url.split("/")[4]
+
+        with open(f"./data/{file_name}.csv", "r", encoding="utf-8", newline="") as file:
+            final_line = file.readlines()[-1]
+            self.start_date = final_line.split(",")[0]
 
     def fetchData(self):
         self.browser.get(self.url)
         arrowBtn = self.browser.find_element(
-            By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/div[1]/div')
+            By.XPATH, '//*[@id="nimbus-app"]/section/section/section/article/div[1]/div[1]/div[1]/button')
         arrowBtn.click()
         maxBtn = self.browser.find_element(
-            By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/section/div[1]/div[1]/div[1]/div/div/div[2]/div/ul[2]/li[4]/button')
+            By.XPATH, '/html/body/div[1]/main/section/section/section/article/div[1]/div[1]/div[1]/div/div/div[2]/section/div[1]/button[8]')
         maxBtn.click()
+        doneBtn = self.browser.find_element(By.XPATH, '//*[@id="menu-50"]/div[2]/section/div[3]/button[1]')
+        doneBtn.click()
 
         table_row = 1
         start_height = 0
@@ -55,12 +66,10 @@ btc_url = "https://finance.yahoo.com/quote/BTC-USD/history?period1=1410912000&pe
 gold_url = "https://finance.yahoo.com/quote/GC%3DF/history"
 eth_url = "https://finance.yahoo.com/quote/ETH-USD/history"
 crude_url = "https://finance.yahoo.com/quote/CL%3DF/history"
-#sp_data = Scraper(sp_url)
-btc_data = Scraper(btc_url)
-eth_data = Scraper(eth_url)
-crude_data = Scraper(crude_url)
-
-
+sp_data = Scraper(sp_url)
+btc_data = Scraper("https://finance.yahoo.com/quote/BTC-USD/history/")
+'''eth_data = Scraper(eth_url)
+crude_data = Scraper(crude_url)'''
 
 
 
