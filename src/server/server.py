d988e253dadce9ec8955d8cbe9250e4863e7d36c
Joshua Obare <jobare.obare@gmail.com>
2024-08-09 18:47:57 +0300

    Restructure server file to work even when models aren't available

--- src/server/server.py
+++ src/server/server.py
@@ -9,26 +9,82 @@ from urllib.parse import urlparse, parse_qs
 import os
 
 
-def load_model(symbol):
-        # Get the directory of the current script
+def get_directories():
+    # Get the directory of the current script
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    
+
     # Construct paths relative to the current script
-    model_path = os.path.join(current_dir, '..', 'finance_ml', 'models', f'{symbol}.pkl')
-    data_path = os.path.join(current_dir, '..', 'finance_ml', 'data', f'{symbol}.csv')
-    # Load your data and model
+    data_path = os.path.join(
+        current_dir, '..', 'finance_ml', 'data')
+
+    # Construct paths relative to the current script
+    model_path = os.path.join(
+        current_dir, '..', 'finance_ml', 'models')
+
+    return data_path, model_path
+
+
+def load_data(symbol):
+    data_path, model_path = get_directories()
+
+    data_path = os.path.join(data_path, f'{symbol}.csv')
+
+    # Load data
+    data = pd.read_csv(f"{data_path}.csv",
+                       index_col="Date", parse_dates=True)
+
+    return data
+
+
+def load_model(symbol):
+    data_path, model_path = get_directories()
+
+    model_path = os.path.join(model_path, f'{symbol}.pkl')
+    # Load model
     with open(model_path, 'rb') as file:
         model = pickle.load(file)
 
-    data = pd.read_csv(data_path,
-                       index_col="Date", parse_dates=True)
-    print(data.index)
-
-    return model, data
+    return model
 
 
 def calculate_mape(actual, predicted):
     return np.mean(np.abs((actual - predicted) / actual)) * 100
+
+
+def generate_data(symbol, model):
+    data = load_data(symbol)
+    # Get the last date in your dataset
+    last_date = data.index[-1]
+
+    # Create a date range for the next day
+    future_dates = pd.date_range(
+        start=last_date + timedelta(days=1), periods=1)
+
+    # Forecast the differenced value for the next day
+    forecast = model.get_forecast(steps=1)
+    forecast_mean = forecast.predicted_mean
+
+    # Get the last actual closing price
+    last_price = data['Close'].iloc[-1]
+
+    # Add the forecasted difference to the last price
+    next_day_price = last_price + forecast_mean.values[0]
+
+    # Get the confidence interval
+    forecast_ci = forecast.conf_int()
+    lower_bound = last_price + forecast_ci.iloc[0, 0]
+    upper_bound = last_price + forecast_ci.iloc[0, 1]
+
+    # Calculate MAPE
+    # We'll use the last 30 days for this example
+    last_30_days = data['Close'].iloc[-30:]
+    last_30_days_forecast = model.get_forecast(steps=30)
+    last_30_days_predicted = last_30_days.iloc[0] + \
+        last_30_days_forecast.predicted_mean.cumsum()
+    mape = calculate_mape(last_30_days.values,
+                          last_30_days_predicted.values)
+
+    return future_dates, next_day_price, lower_bound, upper_bound, mape
 
 
 class PredictionHandler(BaseHTTPRequestHandler):
@@ -44,9 +100,10 @@ class PredictionHandler(BaseHTTPRequestHandler):
         def send_cors_headers(self):
             self.send_header('Access-Control-Allow-Origin', '*')
             self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
-            self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type")
+            self.send_header("Access-Control-Allow-Headers",
+                             "X-Requested-With, Content-Type")
         parsed_path = urlparse(self.path)
-        print(parsed_path)
+
         path_parts = parsed_path.path.split('/')
         if path_parts[1] == 'predict':
             # Extract symbol from the path
@@ -56,59 +113,50 @@ class PredictionHandler(BaseHTTPRequestHandler):
                     symbol = part.split('=')[1]
                     break
 
-            model, data = load_model(symbol)
-            data["Close"] = pd.to_numeric(data["Close"].replace(",", "", regex=True))
-
             if not symbol:
                 self.send_error(400, "Missing 'symbol' parameter")
                 return
-            # Get the last date in your dataset
-            last_date = data.index[-1]
 
-            # Create a date range for the next day
-            future_dates = pd.date_range(
-                start=last_date + timedelta(days=1), periods=1)
+            data = load_data(symbol)
 
-            # Forecast the differenced value for the next day
-            forecast = model.get_forecast(steps=1)
-            forecast_mean = forecast.predicted_mean
+            data["Close"] = pd.to_numeric(
+                data["Close"].replace(",", "", regex=True))
 
-            # Get the last actual closing price
-            last_price = data['Close'].iloc[-1]
-
-            # Add the forecasted difference to the last price
-            next_day_price = last_price + forecast_mean.values[0]
-
-            # Get the confidence interval
-            forecast_ci = forecast.conf_int()
-            lower_bound = last_price + forecast_ci.iloc[0, 0]
-            upper_bound = last_price + forecast_ci.iloc[0, 1]
-
-            # Calculate MAPE
-            # We'll use the last 30 days for this example
-            last_30_days = data['Close'].iloc[-30:]
-            last_30_days_forecast = model.get_forecast(steps=30)
-            last_30_days_predicted = last_30_days.iloc[0] + \
-                last_30_days_forecast.predicted_mean.cumsum()
-            mape = calculate_mape(last_30_days.values,
-                                  last_30_days_predicted.values)
-            
             dataset_dates = data.index.values.tolist()
             # Convert to pandas datetime
             dataset_dates = pd.to_datetime(dataset_dates, unit='ns')
 
             # Convert to list of strings in ISO format
             dataset_dates = dataset_dates.strftime('%Y-%m-%d').tolist()
-            # Prepare the response
-            response = {
-                'data':{"dates":dataset_dates, "data":data['Close'].values.tolist()},
-                'date': future_dates[0].strftime('%Y-%m-%d'),
-                "latest_price": data['Close'][-1],
-                'predicted_price': float(next_day_price),
-                'lower_bound': float(lower_bound),
-                'upper_bound': float(upper_bound),
-                "accuracy": mape,
-            }
+
+            models_available = set(os.listdir(get_directories()[1]))
+
+            if f"{symbol}.pkl" in models_available:
+                model = load_model(symbol)
+
+                future_dates, next_day_price, lower_bound, upper_bound, mape = generate_data(
+                    symbol, model)
+
+                # Prepare the response
+                response = {
+                    'data': {"dates": dataset_dates, "data": data['Close'].values.tolist()},
+                    'date': future_dates[0].strftime('%Y-%m-%d'),
+                    "latest_price": data['Close'][-1],
+                    'predicted_price': float(next_day_price),
+                    'lower_bound': float(lower_bound),
+                    'upper_bound': float(upper_bound),
+                    "price_difference": float(next_day_price) - data['Close'][-1],
+                    "accuracy": mape,
+                    "verdict": "SELL" if float(next_day_price) - data['Close'][-1] < 0 else "BUY"
+                }
+
+            else:
+                # Prepare the response
+                response = {
+                    'data': {"dates": dataset_dates, "data": data['Close'].values.tolist()},
+                    'alert': "Model unavailable"
+                }
+
             self.send_response(200)
             send_cors_headers(self)
             self.send_header('Content-Type', 'application/json')
